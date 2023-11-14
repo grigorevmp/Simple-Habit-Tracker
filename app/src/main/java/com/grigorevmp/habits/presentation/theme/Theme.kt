4456a1a7aaf4e017bf6f85c2132998f90dbfce7e
Mikhail Grigorev <16112000m@gmail.com>
2023-11-14 22:44:51 +0300

    1.0.0-beta-4 | fix: Navigation bart color

--- app/src/main/java/com/grigorevmp/habits/presentation/theme/Theme.kt
+++ app/src/main/java/com/grigorevmp/habits/presentation/theme/Theme.kt
@@ -8,11 +8,13 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -68,6 +70,10 @@ fun HabitTrackerTheme(
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
             systemUiController.setSystemBarsColor(
                 color = colorScheme.background,
+                darkIcons = !darkTheme
+            )
+            systemUiController.setNavigationBarColor(
+                color = colorScheme.surfaceColorAtElevation(3.dp),
                 darkIcons = !darkTheme
             )
         }
